aceb7cc93489ed436ac12447a933aa25f9c73f47
Adir Noyman <ofahol2007@hotmail.com>
2024-11-05 21:49:52 +0200

    .

--- quick-chat/app/page.tsx
+++ quick-chat/app/page.tsx
@@ -1,8 +1,24 @@
+import AddPost from '@/components/AddPost'
+import LeftMenu from '@/components/LeftMenu'
+import PostsFeed from '@/components/PostsFeed'
+import RightMenu from '@/components/RightMenu'
+import Stories from '@/components/Stories'
+import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="">
-      HomePage 🤓
+    <div className='flex gap-6 pt-6'>
+        <div className='hidden xl:block w-[20%]'><LeftMenu /></div>
+        <div className='w-full lg:w-[70%] xl:w-[50%]'>
+          <div className="flex flex-col gap-6">
+            <Stories />
+            <AddPost />
+            <PostsFeed />
+          </div>
+        </div>
+        <div className='hidden lg:block w-[30%]'><RightMenu /></div>
     </div>
-  );
+  )
 }
+
+export default Home
--- quick-chat/components/NavBar.tsx
+++ quick-chat/components/NavBar.tsx
@@ -2,8 +2,13 @@ import Link from 'next/link';
 import React from 'react';
 import MobileMenu from './MobileMenu';
 import Image from 'next/image';
-import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
 const NavBar = () => {
   return (
@@ -18,7 +23,7 @@ const NavBar = () => {
         </Link>
       </div>
       {/* CENTER */}
-      <div className="hidden md:flex w-[50%] text-sm">
+      <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
         {/* LINKS */}
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex items-center gap-2">
@@ -39,7 +44,7 @@ const NavBar = () => {
               height={16}
               className="w-4 h-4"
             />
-            <span>HomePage</span>
+            <span>Friends</span>
           </Link>
           <Link href="/" className="flex items-center gap-2">
             <Image
@@ -49,25 +54,34 @@ const NavBar = () => {
               height={16}
               className="w-4 h-4"
             />
-            <span>HomePage</span>
+            <span>Stories</span>
           </Link>
+        </div>
+        {/* SEARCH */}
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <Image src="/search.png" alt="" width={14} height={14} />
         </div>
       </div>
       {/* RIGHT */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
-      <ClerkLoading>
+        <ClerkLoading>
           <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-blue-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
         </ClerkLoading>
         <ClerkLoaded>
           <SignedIn>
             <div className="cursor-pointer">
-              <Image src="/people.png" alt="" width={24} height={24} />
+              <Image src="/people.png" alt="people" width={24} height={24} />
             </div>
             <div className="cursor-pointer">
-              <Image src="/messages.png" alt="" width={20} height={20} />
+              <Image src="/messages.png" alt="messages" width={20} height={20} />
             </div>
             <div className="cursor-pointer">
-              <Image src="/notifications.png" alt="" width={20} height={20} />
+              <Image src="/notifications.png" alt="notifications" width={20} height={20} />
             </div>
             <UserButton />
           </SignedIn>
